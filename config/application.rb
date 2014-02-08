216b6b58555e80f230149e33ef22817c88673d94
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-02-08 14:42:47 +0100

    fix heroku assets precompiling?

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Raffler
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku rake:assets precompile fix
+    config.assets.initialize_on_precompile = false
   end
 end
